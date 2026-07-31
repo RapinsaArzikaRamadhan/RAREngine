01413517167ea4ce6cc38bc5e9e02f563a2d6501
Rapinsa <Rapinsaarzyka16@gmail.com>
2026-07-31 11:57:35 +0700

    i think the code starting to get complicated for me to work on, i need to start all over again.

--- src/scene/RTransform.cpp
+++
@@ -1,9 +0,0 @@
-#include "scene/RTransform.h"
-#include <raylib.h>
-
-RTransform::RTransform()
-{
-    position = {0.0f, 0.0f, 0.0f};
-    rotation = {0.0f, 0.0f, 0.0f};
-    scale = {1.0f, 1.0f, 1.0f};
-}
